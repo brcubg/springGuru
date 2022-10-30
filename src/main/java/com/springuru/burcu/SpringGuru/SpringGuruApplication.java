3533d34c9046cdfd276eda9ad9944f7312574617
bbh4897 <42186003+bbh4897@users.noreply.github.com>
2022-10-30 18:59:31 +0300

    init

---
+++ src/main/java/com/springuru/burcu/SpringGuru/SpringGuruApplication.java
@@ -0,0 +1,14 @@
+package com.springuru.burcu.SpringGuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuruApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(SpringGuruApplication.class, args);
+	}
+
+}
